09ae8f56714f426893f140f7712a84a52916dca0
Sogyal.T.Sherpa <sogyals429@gmail.com>
2018-12-09 17:32:05 +1100

    Created Signup UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#home'
-  get 'about', to: 'welcome#about'
+  get 'about', to:  'welcome#about'
+  get 'signup', to:  'users#new'
 
   resources :articles
+  resources :users, except: [:new]
+  # post 'users',to: 'users#create'
 
 end
