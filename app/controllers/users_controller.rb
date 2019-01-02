8c73fc2362839865cb75d4797a35a2ac7670a4e0
Sogyal.T.Sherpa <sogyals429@gmail.com>
2019-01-02 14:34:41 +1100

    Completed Auth System

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  
+
+  before_action :set_user, only: [:edit,:update,:show]
+  before_action :require_same_user, only: [:edit,:update]
+
   def index
     @users = User.paginate(page: params[:page], per_page:5)
   end
-  
+
   def new
     @user = User.new
   end
@@ -13,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Alpha Blog#{@user.username}"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -21,11 +24,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to articles_path
@@ -35,7 +37,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page:params[:page], per_page: 5)
   end
 
@@ -43,5 +44,16 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account."
+      redirect_to root_path
+    end
   end
 end
